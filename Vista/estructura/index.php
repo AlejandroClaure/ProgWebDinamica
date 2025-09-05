b1609c634558a1801bf077e868cdfac6b5dc7e92
AlejandroClaure <alejandroclaure01@gmail.com>
2025-09-04 22:47:26 -0300

    meti el index principal en un container de bootstrap

--- Vista/estructura/index.php
+++ Vista/estructura/index.php
@@ -1,6 +1,6 @@
 <?php include("header.php"); ?>
 
-<div class="text-center">
+<div class="container text-center mt-4">
   <!-- Título principal -->
   <h1 class="display-4 mb-4">Bienvenido a los Trabajos Prácticos</h1>
   <p class="lead mb-5">Selecciona un TP desde el menú superior o utiliza los accesos directos.</p>
