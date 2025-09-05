f37d2821003be7eb45fe53304699300f570957d6
AlejandroClaure <alejandroclaure01@gmail.com>
2025-09-05 18:21:36 -0300

    Modificación del index

--- Vista/estructura/index.php
+++ Vista/estructura/index.php
@@ -3,11 +3,11 @@
 <div class="container text-center mt-4">
   <!-- Título principal -->
   <h1 class="display-4 mb-4">Bienvenido a los Trabajos Prácticos</h1>
-  <p class="lead mb-5">Selecciona un TP desde el menú superior o utiliza los accesos directos.</p>
-
+  <h2 class="lead mb-5"><b>Selecciona un TP desde el menú superior.</b></h2>
+<div style="display: none">
   <!-- Accesos rápidos con tarjetas -->
   <div class="row justify-content-center">
-    
+
     <!-- TP1 -->
     <div class="col-md-3">
       <div class="card shadow-sm mb-4">
@@ -39,6 +39,8 @@
     </div>
 
   </div>
+  </div>
+
 </div>
 
 <?php include("footer.php"); ?>
